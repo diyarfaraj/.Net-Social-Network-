ad830c534615a0994820b61a1e5463ffeb89202f
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-03 07:34:41 +0100

    seed some test data into db

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
@@ -9,5 +10,18 @@ namespace Persistence
         {
             
         }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "Value 101"},
+                    new Value {Id = 2, Name = "Value 102"},
+                    new Value {Id = 3, Name = "Value 103"}
+                );
+        }
+        
     }
 }
