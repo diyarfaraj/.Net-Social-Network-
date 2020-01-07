e18595ec18e68fe20c3ef66d4dae1b3d8f231980
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-07 06:31:23 +0100

    added gitignore file

--- Domain/Value.cs
+++ Domain/Value.cs
@@ -4,17 +4,7 @@ namespace Domain
 {
     public class Value
     {
-        private object p1;
-        private object p2;
-
-        public Value(object p1, object p2)
-        {
-            this.p1 = p1;
-            this.p2 = p2;
-        }
-
-        public int Id {get; set;}
-
-        public string Name {get; set;}
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
