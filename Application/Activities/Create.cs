cc516dccd15ce4ca8f7f50deb18a898169fbb1a9
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-14 07:14:16 +0100

    restructure folders

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -6,26 +6,11 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class Edit
+    public class Delete
     {
          public class Command : IRequest 
                 {
-        public Guid Id {get; set;}
-
-        public string Title {get; set;}
-
-        public string Description {get; set;}
-
-        public string Category { get; set; }
-
-        public DateTime? Date { get; set; }
-
-        public string City { get; set; }
-
-        public string Venue { get; set; } 
-                    
-             
-                    
+                    public Guid Id { get; set; }
                 }
         
                 public class Handler : IRequestHandler<Command>
@@ -41,22 +26,16 @@ namespace Application.Activities
                     CancellationToken cancellationToken)
                     {
                       var activity = await _context.Activities.FindAsync(request.Id);
-
                       if(activity == null)
-                      throw new Exception("Could not find activity;)");
+                      throw new Exception("Could not find delete acitvity ;)");
 
-                      activity.Title = request.Title ?? activity.Title;
-                      activity.Description = request.Description ?? activity.Description;
-                      activity.Category = request.Category ?? activity.Category;
-                      activity.Date = request.Date ?? activity.Date;
-                      activity.City = request.City ?? activity.City;
-                      activity.Venue = request.Venue ?? activity.Venue;
+                      _context.Remove(activity);
         
                         var success = await _context.SaveChangesAsync() > 0;
         
                         if(success) return Unit.Value;
         
-                        throw new Exception("Problem Saving chnages");
+                        throw new Exception("Problem Saving delete chnages");
                     }
                 }
     }
