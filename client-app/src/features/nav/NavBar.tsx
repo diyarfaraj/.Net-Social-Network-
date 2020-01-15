d5599a0da34cbdcf5f0baac5df222a23b0f21a81
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-15 17:32:30 +0100

    added navbar

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Menu, Header, Container } from 'semantic-ui-react';
+import { Menu, Header, Container, Button } from 'semantic-ui-react';
 
 export const NavBar = () => {
 	return (
 		<Menu fixed="top" inverted>
 			<Container>
 				<Menu.Item header>
-					<img src="/assets/logo.png" alt="" />
+					<img src="/client-app/public/assets/logo.png" alt="" />
 					Robotia
 				</Menu.Item>
-				<Menu.Item name="messages" />
+				<Menu.Item>
+					<Button positive content="Create Activity" />
+				</Menu.Item>
 				<Menu.Item name="friends" />
 			</Container>
 		</Menu>
