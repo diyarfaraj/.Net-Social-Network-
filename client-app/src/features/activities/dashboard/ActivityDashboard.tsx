e7b19e0cb922d4b17eccba5654a7cfd70413f7d3
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-16 16:28:16 +0100

    added the acitvity list, bug interference

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import { IActivity } from './../../../app/models/activity';
+import { ActivityList } from './ActivityList';
 
 interface IProps {
 	activities: IActivity[];
@@ -11,7 +12,9 @@ export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
 		<Grid>
 			<Grid.Column width={10}>
 				<List>
-					{activities.map((activities) => <List.Item key={activities.id}>{activities.title}</List.Item>)}
+					<ActivityList activities={activities} />
+					{/* 					{activities.map((activities) => <List.Item key={activities.id}>{activities.title}</List.Item>)}
+ */}{' '}
 				</List>
 			</Grid.Column>
 		</Grid>
