81ee680f6ba5d2202341b516c7cb2ac8bcf59482
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-21 07:15:34 +0100

    fill edit forms with current details

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
+import { IActivity } from './../../../app/models/activity';
 
 interface IProps {
 	setEditMode: (editMode: boolean) => void;
+	activity: IActivity;
 }
 
-export const ActivityForm: React.FC<IProps> = ({ setEditMode }) => {
+export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState }) => {
+	const initializeForm = () => {
+		initialFormState
+			? initialFormState
+			: {
+					id: '',
+					title: '',
+					category: '',
+					description: '',
+					date: '',
+					city: '',
+					venue: ''
+				};
+	};
+
+	const [ activity, setActivity ] = useState<IActivity>(initializeForm);
+
 	return (
 		<Segment clearing>
 			<Form>
-				<Form.Input placeholder="Title" />
-				<Form.TextArea placeholder="Description" />
-				<Form.Input placeholder="Category" />
-				<Form.Input type="date" placeholder="Date" />
-				<Form.Input placeholder="City" />
-				<Form.Input placeholder="Venue" />
+				<Form.Input placeholder="Title" value={activity.title} />
+				<Form.TextArea placeholder="Description" value={activity.description} />
+				<Form.Input placeholder="Category" value={activity.category} />
+				<Form.Input type="date" placeholder="Date" value={activity.date} />
+				<Form.Input placeholder="City" value={activity.city} />
+				<Form.Input placeholder="Venue" value={activity.venue} />
 				<Button onClick={() => setEditMode(false)} type="button" floated="right" content="Cancel" />
 				<Button content="Submit" floated="right" positive type="submit" />
 			</Form>
