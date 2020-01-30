3b46411cb6236cde1ae8d463e0ba4a0a26969a7c
Diyar Faraj <diyar.faraj@gmail.com>
2020-01-30 16:26:45 +0100

    added mobx for statemanagement

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
+import React, { useState, useEffect, Fragment, SyntheticEvent, useContext } from 'react';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from './../models/activity';
 import { NavBar } from './../../features/nav/NavBar';
@@ -6,8 +6,11 @@ import agent from '../api/agent';
 
 import { ActivityDashboard } from './../../features/activities/dashboard/ActivityDashboard';
 import { LoadingComponent } from './LoadingComponent';
+import ActivityStore from '../stores/activityStore';
 
 const App = () => {
+	//mobx
+	const activityStore = useContext(ActivityStore);
 	//Hooks
 	const [ activities, setActivities ] = useState<IActivity[]>([]);
 	const [ selectedActivity, setSelectedActivity ] = useState<IActivity | null>(null);
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,8 @@
-import {observable} from 'mobx'
-
+import { observable } from 'mobx';
+import { createContext } from 'react';
 
 class ActivityStore {
-    @observable title = ' Hello from Mobx';
+	@observable title = ' Hello from Mobx';
 }
+
+export default createContext(new ActivityStore());
