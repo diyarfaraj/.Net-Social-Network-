a422b6a1067c16e6840930fd61bad6b35449dc9e
Diyar Faraj <diyar.faraj@gmail.com>
2020-02-13 06:22:13 +0100

    added restexception and error handling middleware

--- Application/Errors/RestException.cs
+++ Application/Errors/RestException.cs
@@ -1,7 +1,16 @@
+using System.Net;
+using System;
 namespace Application.Errors
 {
-    public class RestException
+    public class RestException : Exception
     {
-        
+        public RestException(HttpStatusCode code, object error = null)
+        {
+            Code = code;
+            Errors = error;
+        }
+
+        public HttpStatusCode Code { get; }
+        public object Errors { get; }
     }
 }
