36f713e187419edd4e4157ec094c5cca51611033
Diyar Faraj <diyar.faraj@gmail.com>
2020-02-18 16:50:54 +0100

    seedeb db user and added login handler

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Middleware;
 using Application.Activities;
+using Domain;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -40,6 +42,8 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+
+
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers()
             .AddFluentValidation(cfg => 
@@ -47,6 +51,14 @@ namespace API
                 cfg.RegisterValidatorsFromAssemblyContaining<Create>();
 
             });
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
