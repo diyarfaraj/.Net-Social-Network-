89160185ac026c23d3772e7c4531efe1803ca865
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-21 21:26:36 +0100

    Adding the set main photo functionality

---
+++ Application/Photos/SetMain.cs
@@ -0,0 +1,58 @@
+using System.IO.Compression;
+using System.Linq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Application.Errors;
+using System.Net;
+
+namespace Application.Photos
+{
+    public class SetMain
+    {
+         public class Command : IRequest 
+                {
+                    public string Id { get; set; }
+                }
+        
+                public class Handler : IRequestHandler<Command>
+                {
+                private readonly DataContext _context;
+        private readonly IUserAccessor _userAccessor;
+                    public Handler(DataContext context, IUserAccessor userAccessor)
+                    {
+            _userAccessor = userAccessor;
+                    _context = context;
+                        
+                    }
+        
+                    public async Task<Unit> Handle(Command request, 
+                    CancellationToken cancellationToken)
+                    {
+                      //Handler logic here
+
+                      var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.getCurrentUsername());
+
+                      var photo = user.Photos.FirstOrDefault(x => x.Id == request.Id); 
+
+                      if(photo == null)
+                        throw new RestException(HttpStatusCode.NotFound,  new {Photo = "not Found"}); 
+
+                    var currentMain = user.Photos.FirstOrDefault(x => x.IsMain); 
+
+                    currentMain.IsMain = false; 
+                    photo.IsMain =  true; 
+         
+                        var success = await _context.SaveChangesAsync() > 0;
+        
+                        if(success) return Unit.Value;
+        
+                        throw new Exception("Problem Saving chnages main photo");
+                    }
+                }
+    }
+}
