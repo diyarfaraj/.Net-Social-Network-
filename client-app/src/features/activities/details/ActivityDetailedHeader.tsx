690695aafd05b13ed7f44d4f31ace7aec6d6602f
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-22 13:40:36 +0100

    adding links and profile pag

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -22,6 +22,8 @@ const activityImageTextStyle = {
 const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({ activity }) => {
 	const rootStore = useContext(RootStoreContext);
 	const { attendActivity, cancelAttendence, loading } = rootStore.activityStore;
+	const host = activity.attendees.filter((x) => x.isHost)[0];
+
 	return (
 		<Segment.Group>
 			<Segment basic attached="top" style={{ padding: '0' }}>
@@ -33,7 +35,11 @@ const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({ activity })
 								<Header size="huge" content={activity.title} style={{ color: 'white' }} />
 								<p>{format(activity.date, 'eeee do MMMM')}</p>
 								<p>
-									Hosted by <strong>Bob</strong>
+									Hosted by{' '}
+									<Link to={`/profile/${host.username}`}>
+										{' '}
+										<strong>{host.displayName}</strong>
+									</Link>
 								</p>
 							</Item.Content>
 						</Item>
