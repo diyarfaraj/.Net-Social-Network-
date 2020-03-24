e3d923ce4d7466a27434be0a699bd90752777575
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-24 10:18:56 +0100

    Creating a Photo Upload Widget

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { Tab, Header, Card, Image, Button, Grid } from 'semantic-ui-react';
 import { RootStoreContext } from '../../app/stores/rootStore';
+import PhotoUploadWidget from './../../app/common/PhotoUpload/PhotoUploadWidget';
 
 const ProfilePhotos = () => {
 	const rootStore = useContext(RootStoreContext);
 	const { profile, isCurrentUser } = rootStore.profileStore;
 
-	const [ addPhotoMode, setAddPhotoMode ] = useState(false);
+	const [ addPhotoMode, setAddPhotoMode ] = useState(true);
 	return (
 		<Tab.Pane>
 			<Grid>
@@ -23,7 +24,7 @@ const ProfilePhotos = () => {
 				</Grid.Column>
 				<Grid.Column width={16}>
 					{addPhotoMode ? (
-						<p>Photo Widget will go here</p>
+						<PhotoUploadWidget />
 					) : (
 						<Card.Group itemsPerRow={5}>
 							{profile &&
