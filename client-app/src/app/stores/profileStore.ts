e6f93ecc11c4de027e5b42a62e128aac3d390452
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-28 10:59:31 +0100

    Adding the Photo Upload methods to the store

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,44 +1,71 @@
-import { RootStore } from './rootStore';
-import { observable, action, runInAction, computed } from 'mobx';
-import { IProfile } from '../models/profile';
-import agent from '../api/agent';
+import { RootStore } from "./rootStore";
+import { observable, action, runInAction, computed } from "mobx";
+import { IProfile } from "../models/profile";
+import agent from "../api/agent";
+import { toast } from "react-toastify";
 
 export default class ProfileStore {
-	rootStore: RootStore;
+  rootStore: RootStore;
 
-	constructor(rootStore: RootStore) {
-		this.rootStore = rootStore;
-	}
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+  }
 
-	@observable profile: IProfile | null = null;
-	@observable loadingProfile = true;
+  @observable profile: IProfile | null = null;
+  @observable loadingProfile = true;
+  @observable uploadingPhoto = false;
 
-	@computed
-	get isCurrentUser() {
-		if (this.rootStore.userStore.user && this.profile) {
-			return this.rootStore.userStore.user.userName === this.profile.username;
-		} else {
-			return false;
-		}
-	}
+  @computed
+  get isCurrentUser() {
+    if (this.rootStore.userStore.user && this.profile) {
+      return this.rootStore.userStore.user.userName === this.profile.username;
+    } else {
+      return false;
+    }
+  }
 
-	@action
-	loadProfile = async (username: string) => {
-		this.loadingProfile = true;
+  @action
+  loadProfile = async (username: string) => {
+    this.loadingProfile = true;
 
-		try {
-			const profile = await agent.Profiles.get(username);
+    try {
+      const profile = await agent.Profiles.get(username);
 
-			runInAction(() => {
-				this.profile = profile;
-				this.loadingProfile = false;
-			});
-		} catch (error) {
-			runInAction(() => {
-				this.loadingProfile = false;
-			});
+      runInAction(() => {
+        this.profile = profile;
+        this.loadingProfile = false;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.loadingProfile = false;
+      });
 
-			console.log('error loading profile', error);
-		}
-	};
+      console.log("error loading profile", error);
+    }
+  };
+
+  @action uploadPhoto = async (file: Blob) => {
+    this.uploadingPhoto = true;
+
+    try {
+      const photo = await agent.Profiles.uploadPhoto(file);
+      runInAction(() => {
+        if (this.profile) {
+          this.profile.photos.push(photo);
+          if (photo.isMain && this.rootStore.userStore.user) {
+            this.rootStore.userStore.user.image = photo.url;
+            this.profile.image = photo.url;
+          }
+        }
+
+        this.uploadingPhoto = false;
+      });
+    } catch (error) {
+      console.log("eroror changing the photo", error);
+      toast.error("PRoblem uploading photo");
+      runInAction(() => {
+        this.uploadingPhoto = false;
+      });
+    }
+  };
 }
