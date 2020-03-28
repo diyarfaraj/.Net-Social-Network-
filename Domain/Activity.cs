cbcdd597215c800bbcf195f8cee73f2a3c579853
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-28 20:57:54 +0100

    Adding the Comment entity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,15 +1,13 @@
 using System;
 using System.Collections.Generic;
 
-namespace Domain
-{
-    public class Activity
-    {
-        public Guid Id {get; set;}
+namespace Domain {
+    public class Activity {
+        public Guid Id { get; set; }
 
-        public string Title {get; set;}
+        public string Title { get; set; }
 
-        public string Description {get; set;}
+        public string Description { get; set; }
 
         public string Category { get; set; }
 
@@ -17,7 +15,8 @@ namespace Domain
 
         public string City { get; set; }
 
-        public string Venue { get; set; } 
+        public string Venue { get; set; }
         public virtual ICollection<UserActivity> UserActivities { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
