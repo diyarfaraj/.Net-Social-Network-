6ee38e59a5944ae5d478e4b932a9d6cb9fd733bb
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-28 22:31:16 +0100

    Adding a SignalR hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,21 +1,25 @@
-using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using API.Middleware;
 using Application.Activities;
 using Application.Interfaces;
+using API.Middleware;
+using AutoMapper;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,111 +27,90 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using AutoMapper;
-using Infrastructure.Photos;
 
-
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt => 
-            {
-                opt.UseLazyLoadingProxies();
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (opt => {
+                opt.UseLazyLoadingProxies ();
+                opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            services.AddCors (opt => {
+                opt.AddPolicy ("CorsPolicy", policy => {
+                    policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
                 });
             });
 
+            services.AddMediatR (typeof (List.Handler).Assembly);
+            services.AddAutoMapper (typeof (List.Handler));
+            services.AddSignalR ();
+            services.AddControllers (opt => {
+                    var policy = new AuthorizationPolicyBuilder ().RequireAuthenticatedUser ().Build ();
 
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(List.Handler));
-            services.AddControllers(opt => 
-            {
-                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build
-                ();
+                    opt.Filters.Add (new AuthorizeFilter (policy));
+                })
+                .AddFluentValidation (cfg => {
+                    cfg.RegisterValidatorsFromAssemblyContaining<Create> ();
 
-                opt.Filters.Add(new AuthorizeFilter(policy));
-            })
-            .AddFluentValidation(cfg => 
-            {
-                cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+                });
 
-            });
+            var builder = services.AddIdentityCore<AppUser> ();
+            var identityBuilder = new IdentityBuilder (builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext> ();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>> ();
 
-            var builder = services.AddIdentityCore<AppUser>();
-            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-            identityBuilder.AddEntityFrameworkStores<DataContext>();
-            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
-
-            services.AddAuthorization(opt => 
-            {
-                opt.AddPolicy("IsActivityHost", policy => 
-                {
-                    policy.Requirements.Add(new IsHostRequirement());
+            services.AddAuthorization (opt => {
+                opt.AddPolicy ("IsActivityHost", policy => {
+                    policy.Requirements.Add (new IsHostRequirement ());
                 });
             });
 
-            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler> ();
 
+            var key = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (Configuration["TokenKey"]));
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(opt =>
-                {
-                    opt.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = key,
-                        ValidateAudience = false,
-                        ValidateIssuer = false
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (opt => {
+                    opt.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateAudience = false,
+                    ValidateIssuer = false
                     };
                 });
 
+            services.AddScoped<IJwtGenerator, JwtGenerator> ();
+            services.AddScoped<IUserAccessor, UserAccessor> ();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor> ();
 
-            services.AddScoped<IJwtGenerator, JwtGenerator>();
-            services.AddScoped<IUserAccessor, UserAccessor>();
-            services.AddScoped<IPhotoAccessor, PhotoAccessor>(); 
-            
-            services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+            services.Configure<CloudinarySettings> (Configuration.GetSection ("Cloudinary"));
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            if (env.IsDevelopment())
-            {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseMiddleware<ErrorHandlingMiddleware> ();
+            if (env.IsDevelopment ()) {
                 //app.UseDeveloperExceptionPage();
             }
 
             //app.UseHttpsRedirection();
 
-            app.UseRouting();
-            app.UseCors("CorsPolicy");
-            app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseRouting ();
+            app.UseCors ("CorsPolicy");
+            app.UseAuthentication ();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+                endpoints.MapHub<ChatHub> { "/chat" };
             });
         }
     }
