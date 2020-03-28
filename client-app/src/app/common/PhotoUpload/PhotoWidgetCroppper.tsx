97875312235a626201ca49b2354bbfb13aed5eca
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-28 10:41:04 +0100

    React Cropper Part 2

--- client-app/src/app/common/PhotoUpload/PhotoWidgetCroppper.tsx
+++ client-app/src/app/common/PhotoUpload/PhotoWidgetCroppper.tsx
@@ -1,42 +1,46 @@
-import React, { useRef } from 'react';
-import Cropper from 'react-cropper';
-import 'cropperjs/dist/cropper.css';
+import React, { useRef } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 interface IProps {
-	setImage: (file: Blob) => void;
-	imagePreview: string;
+  setImage: (file: Blob) => void;
+  imagePreview: string;
 }
+
 const PhotoWidgetCroppper: React.FC<IProps> = ({ setImage, imagePreview }) => {
-	const cropper = useRef<Cropper>(null);
+  const cropper = useRef<Cropper>(null);
 
-	const copImage = () => {
-		if (cropper.current && typeof cropper.current.getCroppedCanvas() === 'undefined') {
-			return;
-		}
+  const cropImage = () => {
+    if (
+      cropper.current &&
+      typeof cropper.current.getCroppedCanvas() === "undefined"
+    ) {
+      return;
+    }
 
-		cropper &&
-			cropper.current &&
-			cropper.current.getCroppedCanvas().toBlob((blob: any) => {
-				setImage(blob);
-			}, 'image/jpeg');
-	};
-	return (
-		<Cropper
-			ref={cropper}
-			src={imagePreview}
-			style={{ height: 200, width: '100%' }}
-			// Cropper.js options
-			aspectRatio={1 / 1}
-			guides={false}
-			crop={this._crop.bind(this)}
-			viewMode={1}
-			dragMode="move"
-			scalable={true}
-			copBoxMovable={true}
-			copBoxResizable={true}
-			crop={cropImage}
-		/>
-	);
+    cropper &&
+      cropper.current &&
+      cropper.current.getCroppedCanvas().toBlob((blob: any) => {
+        setImage(blob);
+      }, "image/jpeg");
+  };
+  return (
+    <Cropper
+      ref={cropper}
+      src={imagePreview}
+      style={{ height: 200, width: "100%" }}
+      // Cropper.js options
+      aspectRatio={1 / 1}
+      preview=".img-preview"
+      guides={false}
+      viewMode={1}
+      dragMode="move"
+      scalable={true}
+      cropBoxMovable={true}
+      cropBoxResizable={true}
+      crop={cropImage}
+    />
+  );
 };
 
 export default PhotoWidgetCroppper;
