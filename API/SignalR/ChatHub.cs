203505a88900f3cf47b09fa4ab5dee92559bfd3c
Diyar Faraj <diyar.faraj@gmail.com>
2020-04-01 21:30:01 +0200

    Using SignalR Groups in the API

--- API/SignalR/ChatHub.cs
+++ API/SignalR/ChatHub.cs
@@ -13,7 +13,7 @@ namespace API.SignalR {
         }
 
         public async Task SendComment (Create.Command command) {
-            var username = Context.User?.Claims?.FirstOrDefault (x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            string username = GetUsername ();
 
             command.Username = username;
 
@@ -21,6 +21,26 @@ namespace API.SignalR {
 
             await Clients.All.SendAsync ("RecieveComment", comment);
 
+        }
+
+        private string GetUsername () {
+            return Context.User?.Claims?.FirstOrDefault (x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public async Task AddToGroup (string groupName) {
+            await Groups.AddToGroupAsync (Context.ConnectionId, groupName);
+
+            var username = GetUsername ();
+
+            await Clients.Group (groupName).SendAsync ("Send", $"{username} has joined the group")
+        }
+
+        public async Task RemoveFromGroup (string groupName) {
+            await Groups.RemoveFromGroupAsync (Context.ConnectionId, groupName);
+
+            var username = GetUsername ();
+
+            await Clients.Group (groupName).SendAsync ("Send", $"{username} has left the group")
         }
     }
 }
