a9777a2ac799e53b3525e32fd8f53021dc58307a
Diyar Faraj <diyar.faraj@gmail.com>
2020-04-02 16:47:46 +0200

    Adding the UserFollower Entity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -3,9 +3,12 @@ using Domain;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence {
-    public class DataContext : IdentityDbContext<AppUser> {
-        public DataContext (DbContextOptions options) : base (options) {
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
 
         }
 
@@ -18,26 +21,44 @@ namespace Persistence {
         public DbSet<Photo> Photos { get; set; }
 
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<UserFollowing> Followings { get; set; }
 
-        protected override void OnModelCreating (ModelBuilder builder) {
-            base.OnModelCreating (builder);
-            builder.Entity<Value> ()
-                .HasData (
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Value>()
+                .HasData(
                     new Value { Id = 1, Name = "Value 101" },
                     new Value { Id = 2, Name = "Value 102" },
                     new Value { Id = 3, Name = "Value 103" }
                 );
 
-            builder.Entity<UserActivity> (x => x.HasKey (ua => new { ua.AppUserId, ua.AcitvityId }));
+            builder.Entity<UserActivity>(x => x.HasKey(ua => new { ua.AppUserId, ua.AcitvityId }));
 
-            builder.Entity<UserActivity> ().HasOne (u => u.AppUser)
-                .WithMany (a => a.UserActivities)
-                .HasForeignKey (u => u.AppUserId);
+            builder.Entity<UserActivity>().HasOne(u => u.AppUser)
+                .WithMany(a => a.UserActivities)
+                .HasForeignKey(u => u.AppUserId);
 
-            builder.Entity<UserActivity> ()
-                .HasOne (a => a.Activity)
-                .WithMany (u => u.UserActivities)
-                .HasForeignKey (a => a.AcitvityId);
+            builder.Entity<UserActivity>()
+                .HasOne(a => a.Activity)
+                .WithMany(u => u.UserActivities)
+                .HasForeignKey(a => a.AcitvityId);
+
+
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new { k.ObserverId, k.TargetId });
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasOne(o => o.Target)
+                   .WithMany(f => f.Followers)
+                   .HasForeignKey(o => o.TargetId)
+                   .OnDelete(DeleteBehavior.Restrict);
+            });
         }
 
     }
