5fe9eb3e315400a805d5a515858bf10bcfad9b5f
Diyar Faraj <diyar.faraj@gmail.com>
2020-04-04 19:12:08 +0200

    Swapping our loading indicator for Placeholders

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,32 +1,29 @@
-using System.Collections.Generic;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-using Domain;
-using Application.Activities;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    
     public class ActivitiesController : BaseController
     {
-       
-
-
         [HttpGet]
-        public async Task<ActionResult<List<ActivityDto>>> List()
+        public async Task<ActionResult<List.ActivitiesEnvelope>> List(int? limit,
+            int? offset, bool isGoing, bool isHost, DateTime? startDate)
         {
-            return await Mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query(limit,
+                offset, isGoing, isHost, startDate));
         }
 
         [HttpGet("{id}")]
         [Authorize]
-
         public async Task<ActionResult<ActivityDto>> Details(Guid id)
         {
-            return await Mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
@@ -39,30 +36,27 @@ namespace API.Controllers
         [Authorize(Policy = "IsActivityHost")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
-            command.Id = id; 
+            command.Id = id;
             return await Mediator.Send(command);
         }
 
         [HttpDelete("{id}")]
         [Authorize(Policy = "IsActivityHost")]
-
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            
-            return await Mediator.Send(new Delete.Command{Id = id});
+            return await Mediator.Send(new Delete.Command { Id = id });
         }
 
         [HttpPost("{id}/attend")]
         public async Task<ActionResult<Unit>> Attend(Guid id)
         {
-            return await Mediator.Send(new Attend.Command{Id = id});
+            return await Mediator.Send(new Attend.Command { Id = id });
         }
 
         [HttpDelete("{id}/attend")]
-
         public async Task<ActionResult<Unit>> Unattend(Guid id)
         {
-            return await Mediator.Send(new Unattend.Command{Id = id});
+            return await Mediator.Send(new Unattend.Command { Id = id });
         }
     }
 }
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,6 +6,7 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { RootStoreContext } from "../../../app/stores/rootStore";
 import InfiniteScroll from "react-infinite-scroller";
 import ActivityFilters from "./ActivityFilters";
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder";
 
 const ActivityDashboard: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
@@ -14,7 +15,7 @@ const ActivityDashboard: React.FC = () => {
     loadingInitial,
     setPage,
     page,
-    totalPages
+    totalPages,
   } = rootStore.activityStore;
   const [loadingNext, setLoadingNext] = useState(false);
 
@@ -28,20 +29,21 @@ const ActivityDashboard: React.FC = () => {
     loadActivities();
   }, [loadActivities]);
 
-  if (loadingInitial && page === 0)
-    return <LoadingComponent content="Loading activities" />;
-
   return (
     <Grid>
       <Grid.Column width={10}>
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={handleGetNext}
-          hasMore={!loadingNext && page + 1 < totalPages}
-          initialLoad={false}
-        >
-          <ActivityList />
-        </InfiniteScroll>
+        {loadingInitial && page === 0 ? (
+          <ActivityListItemPlaceholder />
+        ) : (
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={handleGetNext}
+            hasMore={!loadingNext && page + 1 < totalPages}
+            initialLoad={false}
+          >
+            <ActivityList />
+          </InfiniteScroll>
+        )}
       </Grid.Column>
       <Grid.Column width={6}>
         <ActivityFilters />
